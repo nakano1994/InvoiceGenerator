d35c0a6457dd4b7009779716538cf06b72b082f4
Nakano <webnakanotomoki@gmail.com>
2020-01-30 18:14:36 +0900

    change devise/passwords/new.html.slim

--- db/migrate/20200121114427_devise_create_users.rb
+++ db/migrate/20200121114427_devise_create_users.rb
@@ -3,6 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      ## Custom Column
+      t.integer :account_type
+      
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_114427) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.integer "account_type"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
