71253d7c5d2bd90b9ade778aca331f8cb18825f9
Nakano <webnakanotomoki@gmail.com>
2020-02-03 17:20:43 +0900

    add answer new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "sheets#new"
   resources :sheets, except: [:new]
+  resources :sheets do
+    resources :answers
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
